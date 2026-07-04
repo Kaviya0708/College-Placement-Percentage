39c16ca3bde8bd1e76fe6c74171691d07fda491d
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-04 08:20:09 +0530

    Create Main9.java

---
+++ Main9.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        int placed = 240;
+        int totalStudents = 300;
+        int percentage = (placed * 100) / totalStudents;
+
+        System.out.println("Placed Students = " + placed);
+        System.out.println("Total Students = " + totalStudents);
+        System.out.println("Placement Percentage = " + percentage + "%");
+    }
+}
